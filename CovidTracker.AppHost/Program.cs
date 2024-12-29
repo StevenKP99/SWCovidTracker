738d74f3ebb3ff2c452dfacd6c274dc401c8c293
Steven Puckett <steven.k.puckett24@gmail.com>
2024-12-29 11:56:05 -0600

    Change nugetfeed to api key

--- CovidTracker.AppHost/Program.cs
+++ CovidTracker.AppHost/Program.cs
@@ -1,10 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.CovidTracker_ApiService>("apiservice");
+var apiService = builder.AddProject<Projects.CovidTracker_ApiService>("apiservice")
+    .WithExternalHttpEndpoints();
 
 builder.AddProject<Projects.CovidTracker_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService)
-    .WaitFor(apiService);
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
